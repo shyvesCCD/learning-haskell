559b0743077040f3e0b1756e48fe19b93cc59221
shyvesCCD <pedroochavess97@gmail.com>
2021-11-08 23:39:38 -0300

    Aprendendo algumas coisas.

---
+++ src/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+somatoria :: [Int] -> Int
+somatoria = sum 
+
+f :: Double -> Double -> Double
+f x y = z+w
+  where z = g x
+        w = y+y
+        g x' = x'*3
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
